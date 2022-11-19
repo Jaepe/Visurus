4a15e8a0dbb22634a06c94f10a0ee0de963a4709
Jaepe <100645885+Jaepe@users.noreply.github.com>
2022-11-19 01:04:33 -0500

    1.0.0

---
+++ Games/AdventureGames.lua
@@ -0,0 +1,7 @@
+return {
+	[[{"Name":"Doors GUI","gameId":6516141723,"script":"https://raw.githubusercontent.com/RegularVynixu/Vynixius/main/Doors/Script.lua","credits":"someone"}]],
+	[[{"Name":"Doors Skip (press h to skip room)","gameId":6516141723,"script":"https://pastebin.com/raw/pBKpQDux","credits":"RegularVynixu#8039"}]],
+	[[{"Name":"Rainbow Friends","gameId":7991339063,"script":"https://raw.githubusercontent.com/TweedLeak/Scripts/main/Rainbow-Friends.lua"}]],
+	[[{"Name":"Apeirophobia (Run When not in lobby)","gameId":10277635599,"script":"https://pastebin.com/raw/mVkMT64F"}]],
+	[[{"Name":"Break in","gameId":3851622790,"script":"https://raw.githubusercontent.com/TrixAde/Proxima-Hub/main/Main.lua"}]],
+}
---
+++ Games/RoleplayGames.lua
@@ -0,0 +1,3 @@
+return {
+	[[{"Name":"Brookhaven","gameId":4924922222,"script":"https://raw.githubusercontent.com/IceMael7/NewIceHub/main/Brookhaven"}]],
+}
